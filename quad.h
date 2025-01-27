a07e6472bdf5489096f6fdc3eb551faf1c9db580
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2025-01-27 17:45:19 +0000

    Remove unused functions and streamline quad.h for improved clarity and maintainability

--- quad.h
+++ quad.h
@@ -35,19 +35,9 @@ void so3hat(const double* v, double* result) {
     result[6]=-v[1]; result[7]=v[0]; result[8]=0;
 }
 
-void so3vee(const double* m, double* result) {
-    result[0] = m[7];
-    result[1] = m[2];
-    result[2] = m[3];
-}
-
 // Matrix arithmetic
 void addMat3f(const double* a, const double* b, double* result) {
     for(int i = 0; i < 9; i++) result[i] = a[i] + b[i];
-}
-
-void subMat3f(const double* a, const double* b, double* result) {
-    for(int i = 0; i < 9; i++) result[i] = a[i] - b[i];
 }
 
 void multScalMat3f(double s, const double* m, double* result) {
@@ -75,63 +65,6 @@ void subVec3f(const double* a, const double* b, double* result) {
 
 double dotVec3f(const double* a, const double* b) {
     return a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
-}
-
-void normVec3f(const double* v, double* result) {
-    double mag = sqrtf(dotVec3f(v, v));
-    for(int i = 0; i < 3; i++) result[i] = v[i]/mag;
-}
-
-void inv4Mat4f(const double* m, double* result) {
-    double s0 = m[0]*m[5] - m[4]*m[1];
-    double s1 = m[0]*m[6] - m[4]*m[2];
-    double s2 = m[0]*m[7] - m[4]*m[3];
-    double s3 = m[1]*m[6] - m[5]*m[2];
-    double s4 = m[1]*m[7] - m[5]*m[3];
-    double s5 = m[2]*m[7] - m[6]*m[3];
-
-    double c5 = m[10]*m[15] - m[14]*m[11];
-    double c4 = m[9]*m[15] - m[13]*m[11];
-    double c3 = m[9]*m[14] - m[13]*m[10];
-    double c2 = m[8]*m[15] - m[12]*m[11];
-    double c1 = m[8]*m[14] - m[12]*m[10];
-    double c0 = m[8]*m[13] - m[12]*m[9];
-
-    double det = s0*c5 - s1*c4 + s2*c3 + s3*c2 - s4*c1 + s5*c0;
-    
-    if (det == 0.0) {
-        // Handle error case
-        return;
-    }
-
-    double invdet = 1.0/det;
-
-    result[0] = (m[5]*c5 - m[6]*c4 + m[7]*c3)*invdet;
-    result[1] = (-m[1]*c5 + m[2]*c4 - m[3]*c3)*invdet;
-    result[2] = (m[13]*s5 - m[14]*s4 + m[15]*s3)*invdet;
-    result[3] = (-m[9]*s5 + m[10]*s4 - m[11]*s3)*invdet;
-
-    result[4] = (-m[4]*c5 + m[6]*c2 - m[7]*c1)*invdet;
-    result[5] = (m[0]*c5 - m[2]*c2 + m[3]*c1)*invdet;
-    result[6] = (-m[12]*s5 + m[14]*s2 - m[15]*s1)*invdet;
-    result[7] = (m[8]*s5 - m[10]*s2 + m[11]*s1)*invdet;
-
-    result[8] = (m[4]*c4 - m[5]*c2 + m[7]*c0)*invdet;
-    result[9] = (-m[0]*c4 + m[1]*c2 - m[3]*c0)*invdet;
-    result[10] = (m[12]*s4 - m[13]*s2 + m[15]*s0)*invdet;
-    result[11] = (-m[8]*s4 + m[9]*s2 - m[11]*s0)*invdet;
-
-    result[12] = (-m[4]*c3 + m[5]*c1 - m[6]*c0)*invdet;
-    result[13] = (m[0]*c3 - m[1]*c1 + m[2]*c0)*invdet;
-    result[14] = (-m[12]*s3 + m[13]*s1 - m[14]*s0)*invdet;
-    result[15] = (m[8]*s3 - m[9]*s1 + m[10]*s0)*invdet;
-}
-
-void multMatVec4f(const double* m, const double* v, double* result) {
-    result[0] = m[0]*v[0] + m[1]*v[1] + m[2]*v[2] + m[3]*v[3];
-    result[1] = m[4]*v[0] + m[5]*v[1] + m[6]*v[2] + m[7]*v[3];
-    result[2] = m[8]*v[0] + m[9]*v[1] + m[10]*v[2] + m[11]*v[3];
-    result[3] = m[12]*v[0] + m[13]*v[1] + m[14]*v[2] + m[15]*v[3];
 }
 
 void orthonormalize_rotation_matrix(double* R) {
@@ -233,17 +166,6 @@ void get_quad_state(Quad q, double* state) {
     state[11] = q.R_W_B[8];
 }
 
-void print_quad(Quad q) {
-    printf("\rP: %.2f %.2f %.2f | R: %.2f %.2f %.2f %.2f", 
-           q.linear_position_W[0], 
-           q.linear_position_W[1], 
-           q.linear_position_W[2], 
-           q.omega[0], 
-           q.omega[1], 
-           q.omega[2], 
-           q.omega[3]);
-}
-
 static double gaussian_noise(double stddev) {
     double u1 = (double)rand() / RAND_MAX, u2 = (double)rand() / RAND_MAX;
     return sqrt(-2.0 * log(u1)) * cos(2.0 * M_PI * u2) * stddev;
@@ -340,123 +262,6 @@ void update_quad(Quad* q, double dt) {
     // 11. Update rotor speeds
     for(int i = 0; i < 4; i++) {
         q->omega[i] = fmax(OMEGA_MIN, fmin(OMEGA_MAX, q->omega_next[i]));
-    }
-}
-
-void control_quad(Quad* q, double* control_input) {
-    // 1. Calculate position and velocity errors
-    double error_p[3], error_v[3];
-    subVec3f(q->linear_position_W, (double[]){control_input[0], control_input[1], control_input[2]}, error_p);
-    subVec3f(q->linear_velocity_W, (double[]){control_input[3], control_input[4], control_input[5]}, error_v);
-
-    // 2. Calculate desired force vector in world frame
-    double z_W_d[3], temp[3];
-    multScalVec3f(-K_P, error_p, z_W_d);
-    multScalVec3f(-K_V, error_v, temp);
-    addVec3f(z_W_d, temp, z_W_d);
-    
-    // Add gravity compensation and desired acceleration
-    double gravity_term[3] = {0, MASS * GRAVITY, 0};
-    addVec3f(z_W_d, gravity_term, z_W_d);
-    
-    double accel_term[3];
-    multScalVec3f(MASS, (double[]){0.0, 0.0, 0.0}, accel_term);
-    addVec3f(z_W_d, accel_term, z_W_d);
-
-    // 3. Calculate thrust magnitude
-    double z_W_B[3];
-    double y_body[3] = {0, 1, 0};
-    multMatVec3f(q->R_W_B, y_body, z_W_B);
-    control_input[0] = dotVec3f(z_W_d, z_W_B);
-
-    // 4. Calculate desired rotation matrix
-    double x_tilde_d_W[3] = {sin(control_input[6]), 0.0, cos(control_input[6])};
-    double temp_cross1[3], temp_cross2[3];
-    double R_W_d_column_0[3], R_W_d_column_1[3], R_W_d_column_2[3];
-    
-    crossVec3f(z_W_d, x_tilde_d_W, temp_cross1);
-    crossVec3f(temp_cross1, z_W_d, temp_cross2);
-    normVec3f(temp_cross2, R_W_d_column_0);
-    normVec3f(temp_cross1, R_W_d_column_1);
-    normVec3f(z_W_d, R_W_d_column_2);
-
-    double R_W_d[9] = {
-        R_W_d_column_1[0], R_W_d_column_2[0], R_W_d_column_0[0],
-        R_W_d_column_1[1], R_W_d_column_2[1], R_W_d_column_0[1],
-        R_W_d_column_1[2], R_W_d_column_2[2], R_W_d_column_0[2]
-    };
-
-    // 5. Calculate rotation error
-    double R_W_d_T[9], R_W_B_T[9], temp_mat1[9], temp_mat2[9], temp_mat3[9];
-    transpMat3f(R_W_d, R_W_d_T);
-    transpMat3f(q->R_W_B, R_W_B_T);
-
-    multMat3f(R_W_d_T, q->R_W_B, temp_mat1);
-    multMat3f(R_W_B_T, R_W_d, temp_mat2);
-    subMat3f(temp_mat1, temp_mat2, temp_mat3);
-
-    double error_r[3];
-    so3vee(temp_mat3, error_r);
-    multScalVec3f(0.5, error_r, error_r);
-
-    // 6. Calculate angular velocity error
-    double temp_vec[3], error_w[3];
-    multMat3f(R_W_d_T, q->R_W_B, temp_mat1);
-    multMatVec3f(temp_mat1, (double[]){0.0, 0.0, 0.0}, temp_vec);
-    subVec3f(q->angular_velocity_B, temp_vec, error_w);
-
-    // 7. Calculate control torque
-    double tau_B_control[3], temp_vec2[3];
-    multScalVec3f(-K_R, error_r, tau_B_control);
-    multScalVec3f(-K_W, error_w, temp_vec2);
-    addVec3f(tau_B_control, temp_vec2, tau_B_control);
-
-    // Add angular momentum terms
-    double I_mat[9], temp_vec3[3], temp_vec4[3];
-    vecToDiagMat3f(q->inertia, I_mat);
-    multMatVec3f(I_mat, q->angular_velocity_B, temp_vec3);
-    crossVec3f(q->angular_velocity_B, temp_vec3, temp_vec4);
-    addVec3f(tau_B_control, temp_vec4, tau_B_control);
-
-    // Add feedforward terms
-    double term_0[3], term_1[3], temp_vec5[3];
-    multMatVec3f(R_W_d, (double[]){0.0, 0.0, 0.0}, temp_vec);
-    multMatVec3f(R_W_B_T, temp_vec, term_0);
-
-    multMatVec3f(R_W_d, (double[]){0.0, 0.0, 0.0}, temp_vec);
-    multMatVec3f(R_W_B_T, temp_vec, temp_vec2);
-    crossVec3f((double[]){0.0, 0.0, 0.0}, temp_vec2, term_1);
-
-    subVec3f(term_1, term_0, temp_vec5);
-    multMatVec3f(I_mat, temp_vec5, temp_vec);
-    subVec3f(tau_B_control, temp_vec, tau_B_control);
-
-    // 8. Calculate rotor speeds
-    double F_bar[16] = {
-        K_F, K_F, K_F, K_F,   // Thrust coefficients
-        0, 0, 0, 0,           // Roll moments
-        K_M, -K_M, K_M, -K_M, // Yaw moments
-        0, 0, 0, 0            // Pitch moments
-    };
-
-    // Calculate roll and pitch moments
-    for(int i = 0; i < 4; i++) {
-        double moment[3];
-        double pos_scaled[3];
-        multScalVec3f(K_F, (double [4][3]){{-L, 0,  L}, { L, 0,  L}, { L, 0, -L}, {-L, 0, -L}}[i], pos_scaled);
-        crossVec3f(pos_scaled, (double[3]){0, 1, 0}, moment);
-        F_bar[4 + i]  = moment[0];  // Roll
-        F_bar[12 + i] = moment[2];  // Pitch
-    }
-
-    // 9. Calculate and update rotor speeds
-    double F_bar_inv[16];
-    inv4Mat4f(F_bar, F_bar_inv);
-    double omega_sign_square[4];
-    multMatVec4f(F_bar_inv, (double[]){control_input[0], tau_B_control[0], tau_B_control[1], tau_B_control[2]}, omega_sign_square);
-
-    for(int i = 0; i < 4; i++) {
-        q->omega_next[i] = sqrt(fabs(omega_sign_square[i]));
     }
 }
 
