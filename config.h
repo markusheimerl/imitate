1c84d8daac57fbd92a4eeeb973503c5057518958
Markus Heimerl <149831926+markusheimerl@users.noreply.github.com>
2025-01-28 12:48:13 +0000

    Reduce history length and adjust frequency and rollout parameters for optimized performance

--- config.h
+++ config.h
@@ -9,14 +9,14 @@
 
 #define STATE_DIM 6  // 3 accel + 3 gyro
 #define ACTION_DIM 8
-#define HISTORY_LENGTH 16
+#define HISTORY_LENGTH 8
 // At 60Hz control rate (DT_CONTROL = 1/60s), taking every 8th state means
 // each history sample is 8/60 = 0.133s apart. With HISTORY_LENGTH = 16,
 // this gives us a total history window of 16 * 8/60 = 2.133s
-#define HISTORY_FREQUENCY 8
+#define HISTORY_FREQUENCY 64
 #define TOTAL_STATE_DIM (STATE_DIM * HISTORY_LENGTH)
 #define MAX_STEPS 1000
-#define NUM_ROLLOUTS 128
+#define NUM_ROLLOUTS 32
 
 #define GAMMA 0.999
 #define MAX_STD 3.0
